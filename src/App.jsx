5cbf1078425673dae66026c7d0b4ef1c2e67f7fb
Nytrix-09 <75318042+Nytrix-09@users.noreply.github.com>
2024-09-21 15:29:37 -0400

    first-commit-for-beta-testing

---
+++ src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+import './App.css'
+import Search from './assets/search.png'
+import MovieCard from './MovieCard'
+
+const API_URL = 'https://omdbapi.com/?apikey=5cdc3843'
+
+function App() {
+  const [movies, setMovies] = useState([])
+  const [searchMovie, setSearchMovie] = useState("")
+
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json()
+
+    setMovies(data.Search);
+  }
+
+  const handleSearch = (e) => { 
+    if(e.key === 'Enter') {
+      searchMovies(searchMovie)
+    }
+  };
+
+  return (
+    <>
+      <div className="app">
+        <h1>Aj's Movie Finder</h1>
+
+        <div className="search">
+          <input placeholder="Search For A Movie/TV show..." value={searchMovie} onChange={(e) => setSearchMovie(e.target.value)} onKeyDown={handleSearch}/>
+          <img src={Search} alt="Search Icon" onClick={() => searchMovies(searchMovie)}/>
+        </div>
+
+        {movies?.length > 0 ? (
+          <div className="container">
+            {movies.map((movie, index) => (
+              <MovieCard key={index} movie={movie} />
+            ))}
+          </div>
+        ): (
+          <div className="empty">
+            <h2>Search for your favorite Movies and Series</h2>
+          </div>
+        )}
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/MovieCard.jsx
@@ -0,0 +1,22 @@
+
+const MovieCard = ({ movie }) => {
+    return(
+        <div className="movie">
+            <div>
+                <h3>{movie.Title}</h3>
+            </div>
+            <div>
+                <img src={movie.Poster} alt={movie.Title} />
+            </div>
+
+            <div>
+                <span>{movie.Type}</span>
+                <h3>{movie.Titleype}</h3>
+                <h3>{movie.Year}</h3>
+            </div>
+            
+        </div>
+    )
+}
+
+export default MovieCard
